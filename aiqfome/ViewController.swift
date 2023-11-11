ca8ff8d2f7ccf7bbfa08ffe462650c8a465fbf60
Frédéric Helfer <fre.helfer@gmail.com>
2023-11-11 16:06:41 -0300

    Initial Commit

---
+++ aiqfome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aiqfome
+//
+//  Created by Frédéric Helfer on 11/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
